87a1cf9a33a0d51fc62a809c87f0be71646b5a65
Chima Ejiofor <mobiblunt@gmail.com>
2024-07-25 16:40:03 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
+const { PrismaClient } = require('@prisma/client')
+// 1
+
+const prisma = new PrismaClient()
+
+
+  
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
+  },
+
+  Mutation: {
+    // 2
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    }
+  },
+}
+
+// 3
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+      ),
+  resolvers,
+  context: {
+    prisma,
+  }
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
